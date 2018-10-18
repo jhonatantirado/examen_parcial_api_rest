04a81c1b4bb35ee594ab5605d0d77f7968920434
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2018-10-18 16:36:28 -0500

    RabbitMQ and Assembler pattern

---
+++ Api/Customers/Application/Assembler/CustomerProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EnterprisePatterns.Api.Customers.Application.Dto;
+
+namespace EnterprisePatterns.Api.Customers.Application.Assembler
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, CustomerDto>()
+                .ForMember(
+                    dest => dest.OrganizationName,
+                    x => x.MapFrom(src => src.OrganizationName)
+                );
+            CreateMap<SignUpDto, Customer>()
+                .ForMember(
+                    dest => dest.OrganizationName,
+                    x => x.MapFrom(src => src.OrganizationName)
+                );
+        }
+    }
+}
