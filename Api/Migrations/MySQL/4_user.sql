26dbf8e0d01658e43cab99ce6e19c92483a5601c
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2018-10-19 18:29:44 -0500

    Username should be UNIQUE

--- Api/Migrations/MySQL/4_user.sql
+++ Api/Migrations/MySQL/4_user.sql
@@ -6,6 +6,7 @@ CREATE TABLE user (
   customer_id bigint UNSIGNED NOT NULL,
   PRIMARY KEY (user_id),
   UNIQUE KEY user_id_UNIQUE (user_id),
+  UNIQUE KEY username_UNIQUE (username),
   CONSTRAINT FK_user_role_id FOREIGN KEY(role_id) REFERENCES role(role_id),
   CONSTRAINT FK_user_customer_id FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
